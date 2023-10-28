ada0e33b41ca68605898771b37ab80df5b581694
Gollavilli Dhanush Kumar <112778628+Dhanush2468@users.noreply.github.com>
2023-10-28 20:40:24 +0530

    v3

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import * as dotenv from "dotenv";
 import cors from "cors";
 
 import connectDB from "./mongodb/connect.js";
+import postRoutes from './routes/postRoutes.js'
+import devilRoutes from './routes/devilRoutes.js'
 
 dotenv.config();
 
@@ -11,8 +13,8 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 // // Routes
-// app.use('/api/v1/post', postRoutes);
-// app.use('/api/v1/dalle', dalleRoutes);
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/devil', devilRoutes);
 
 app.get("/", async (req, res) => {
   res.send("Hello From DEVIL-AI!");
